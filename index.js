c3087b3a3cf0373008a786b4d88e1f7e300173ea
Jefferson Sombra <soombra73@gmail.com>
2022-02-19 02:38:49 -0300

    Utilizando Async e Await

--- index.js
+++ index.js
@@ -2,18 +2,39 @@ const chalk = require('chalk');
 const fs = require('fs');
 
 function trataErro (erro) {
-    throw new Error(chalk.white.bgRedBright.bold(erro.code, 'Não há arquivos no caminho')); //throw new Error, é como deve ser tratado uma mensagem de erro. erro.code mostra o código do erro, para ficar mais fácil de verificar na documentação
+    throw new Error(chalk.white.bgRedBright.bold(erro.code, 'Não há arquivos no caminho informado')); //throw new Error, é como deve ser tratado uma mensagem de erro. erro.code mostra o código do erro, para ficar mais fácil de verificar na documentação
 }
 
-function pegaArquivo (caminhoDoArquivo) {
-    const encoding = 'utf-8'
-    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-        if (erro) {
-            trataErro(erro)
-        } else {
-            console.log(chalk.black.bgGreenBright(texto));
-        }
-    })
+async function pegaArquivo (caminhoDoArquivo) {
+    const encoding = 'utf-8';
+    try {
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+        console.log(chalk.black.bgGreenBright(texto));
+    } catch (erro) {
+        trataErro(chalk.white.bgRedBright(erro))
+    }
 }
+
+
+// function pegaArquivo (caminhoDoArquivo) {
+//     const encoding = 'utf-8'
+//     fs.promises
+//     .readFile(caminhoDoArquivo, encoding)
+//     .then((texto) => console.log(chalk.black.bgGreenBright(texto)))//método callback, recebe uma função como parâmetro o produto da promessa 
+//     .catch((erro) => trataErro(erro))//método para capturar erros em promessas
+// }
+
+
+// function pegaArquivo (caminhoDoArquivo) {
+//     const encoding = 'utf-8'
+//     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+//         if (erro) {
+//             trataErro(erro)
+//         } else {
+//             console.log(chalk.black.bgGreenBright(texto));
+//         }
+//     })
+// }
 
 pegaArquivo('./arquivos/texto1.md')
+pegaArquivo('./arquivos/')
