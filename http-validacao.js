3612c63b7736d26c0db6494e9327b3cacfddab81
Jefferson Sombra <soombra73@gmail.com>
2022-02-20 21:34:51 -0300

    Aula 5 - Validando Links. Lib agora está utilizando o node-fatch para validar os links que estão no arquivo .md. E retornar o statuscode da página, e a mensagem que diz o que significa o número do status.

--- http-validacao.js
+++ http-validacao.js
@@ -1,1 +1,39 @@
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const fetch = require('node-fetch');
+
+function tratarErros (erro) {
+    throw new Error(erro.message);    
+}
+
+
+async function checaStatus (arrayURLs) {
+    try{
+        const arrayStatus = await Promise
+            .all(arrayURLs
+                .map(async url => {
+                const res = await fetch(url);
+                return `${res.status} - ${res.statusText}`;
+    }))
+    return arrayStatus;
+    } catch(erro){
+        tratarErros(erro);
+    }
+}
+
+function geraArrayDeURLs (arrayLinks) {
+    return arrayLinks
+    .map(objetoLink => Object
+        .values(objetoLink).join())
+}
+
+async function validaURLs (arrayLinks) {
+   const links = geraArrayDeURLs(arrayLinks);
+   const statusLinks = await checaStatus(links); 
+   let indice = 0
+   const resultados = arrayLinks.map((objeto, indice) => ({
+       ...objeto, 
+       statusURLs: statusLinks[indice]
+   }))
+   return resultados;
+}
+
+module.exports = validaURLs;
--- index.js
+++ index.js
@@ -19,20 +19,33 @@ function trataErro (erro) {
     throw new Error(chalk.white.bgRedBright.bold(erro.code, 'Não há arquivos no caminho informado')); //throw new Error, é como deve ser tratado uma mensagem de erro. erro.code mostra o código do erro, para ficar mais fácil de verificar na documentação
 }
 
-async function pegaArquivo(caminho) {
-    const caminhoAbsoluto = path.join(__dirname, '..', caminho);
-    const encoding = 'utf-8';
+async function pegaArquivo (caminhoDoArquivo) {
+    const enconding = 'utf-8';
     try {
-    const arquivos = await fs.promises.readdir(caminhoAbsoluto, { encoding });
-    const result = await Promise.all(arquivos.map(async (arquivo) => {
-      const localArquivo = `${caminhoAbsoluto}/${arquivo}`;
-      const texto = await fs.promises.readFile(localArquivo, encoding);
-      return extraiLinks(texto);
-    }));
-    return result
-   } catch {
-       return trataErro(erro)
-   }
+        const texto = await fs.promises.readFile(caminhoDoArquivo, enconding)
+        return extraiLinks(texto);
+      } catch(erro) {
+        trataErro(erro);
+      }
 }
+
+// ---Função para buscar todos os arquivos dentro de uma pasta
+// async function pegaArquivoDiretorio(caminho) {
+//     const caminhoAbsoluto = path.join(__dirname, '..', caminho);
+//     const encoding = 'utf-8';
+//     try {
+//     const arquivos = await fs.promises.readdir(caminhoAbsoluto, {encoding});
+//     const result = await Promise.all(arquivos.map(async (arquivo) => {
+//       const localArquivo = `${caminhoAbsoluto}/${arquivo}`;
+//       const texto = await fs.promises.readFile(localArquivo, encoding);
+//       return extraiLinks(texto);
+//     }));
+//     return result
+//    } catch {
+//        return trataErro(erro)
+//    }
+// }
+// module.exports = pegaArquivoDiretorio;
+
 
 module.exports = pegaArquivo;
